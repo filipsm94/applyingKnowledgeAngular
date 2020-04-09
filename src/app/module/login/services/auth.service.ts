740cf514c4369dd4e9a08730677eee6333159ef6
Filipsm94 <filipsm94@gmail.com>
2020-04-09 17:48:20 -0500

    creating service authenticated

--- src/app/module/login/services/auth.service.ts
+++ src/app/module/login/services/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IUser } from 'src/app/shared/models/user.model';
+import { UrlsAplicationConstants } from 'src/app/shared/constants/ulrs-aplication.constans';
+import { AuthServiceType } from './auth.service.type';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements AuthServiceType {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  public isUserActive(user: IUser): Promise<IUser>{
+    return this._http.post(UrlsAplicationConstants.URL_SERVICE_IS_USER_ACTIVE, user).pipe().toPromise();
+  }
 }
