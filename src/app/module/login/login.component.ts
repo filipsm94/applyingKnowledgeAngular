4878de092703c3e4387ae0977d2cfe5de598700f
Filipsm94 <filipsm94@gmail.com>
2020-04-09 17:48:01 -0500

    config module login

--- src/app/module/login/login.component.ts
+++ src/app/module/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+import { IUser } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+    ) {
+    this.loginForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.maxLength(12), Validators.minLength(2)]],
+      pass: [null, Validators.required]
+    })
+  }
+
+  public ngOnInit(): void {
+  }
+
+  public async loginAction(){
+    console.log(this.loginForm);
+    const user: IUser={
+      username: this.loginForm.get('name').value,
+      pass: this.loginForm.get('pass').value,
+
+    }
+    const response = await this.authService.isUserActive(user);
+    console.log(response);
+    
   }
 
 }
--- src/app/module/login/login.module.ts
+++ src/app/module/login/login.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers:[
+    AuthService
   ]
 })
 export class LoginModule { }
